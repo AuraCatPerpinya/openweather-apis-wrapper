19e6762320c23a1b648f8182e2dd0bbd0130d4eb
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-08-15 22:41:10 +0200

    feat(cache): caches can be disabled now

--- src/cache/cache.ts
+++ src/cache/cache.ts
@@ -21,46 +21,66 @@ export interface Cache<TCached> {
 }
 
 export interface Caches {
-  coordinatesByLocationName: Cache<CoordinatesByLocationName[]>;
-  coordinatesByZipOrPostCode: Cache<CoordinatesByZipOrPostCode>;
-  locationNameByCoordinates: Cache<LocationNameByCoordinates[]>;
-  currentWeather: Cache<CurrentWeather>;
-  forecast5days3hours: Cache<Forecast5days3hours>;
+  coordinatesByLocationName?: Cache<CoordinatesByLocationName[]>;
+  coordinatesByZipOrPostCode?: Cache<CoordinatesByZipOrPostCode>;
+  locationNameByCoordinates?: Cache<LocationNameByCoordinates[]>;
+  currentWeather?: Cache<CurrentWeather>;
+  forecast5days3hours?: Cache<Forecast5days3hours>;
 }
 
-export type OptionalizedCaches = { [key in keyof Caches]?: Caches[key] };
+export type OptionalizedCaches = {
+  [key in keyof Caches]: Caches[key] | null;
+};
 export type CacheHandlerOptions = {
-  /** (optional) - You can instantiate custom caches here */
+  /** (optional) - You can instantiate custom caches here.
+   *
+   * Set a cache to null (not undefined, null, very important!) to completely disable it.
+   */
   caches?: OptionalizedCaches;
 };
 
 export class CacheHandler implements Caches {
-  coordinatesByLocationName: Cache<CoordinatesByLocationName[]>;
-  coordinatesByZipOrPostCode: Cache<CoordinatesByZipOrPostCode>;
-  locationNameByCoordinates: Cache<LocationNameByCoordinates[]>;
-  currentWeather: Cache<CurrentWeather>;
-  forecast5days3hours: Cache<Forecast5days3hours>;
+  coordinatesByLocationName?: Cache<CoordinatesByLocationName[]>;
+  coordinatesByZipOrPostCode?: Cache<CoordinatesByZipOrPostCode>;
+  locationNameByCoordinates?: Cache<LocationNameByCoordinates[]>;
+  currentWeather?: Cache<CurrentWeather>;
+  forecast5days3hours?: Cache<Forecast5days3hours>;
 
   constructor(options?: CacheHandlerOptions) {
     const caches = options?.caches;
 
-    this.coordinatesByLocationName = caches?.coordinatesByLocationName ||
-      new MemoryCache(
-        { sweepDelay: 30 * 1000 * 60 },
-      );
-    this.coordinatesByZipOrPostCode = caches?.coordinatesByZipOrPostCode ||
-      new MemoryCache(
-        { sweepDelay: 30 * 1000 * 60 },
-      );
-    this.locationNameByCoordinates = caches?.locationNameByCoordinates ||
-      new MemoryCache(
-        { sweepDelay: 30 * 1000 * 60 },
-      );
-    this.currentWeather = caches?.currentWeather || new MemoryCache(
-      { sweepDelay: 10 * 1000 * 60 },
-    );
-    this.forecast5days3hours = caches?.forecast5days3hours || new MemoryCache(
-      { sweepDelay: 10 * 1000 * 60 },
-    );
+    this.coordinatesByLocationName =
+      caches && (caches.coordinatesByLocationName === null)
+        ? undefined
+        : caches?.coordinatesByLocationName ||
+          new MemoryCache(
+            { sweepDelay: 30 * 1000 * 60 },
+          );
+    this.coordinatesByZipOrPostCode =
+      caches && (caches.coordinatesByZipOrPostCode === null)
+        ? undefined
+        : caches?.coordinatesByZipOrPostCode ||
+          new MemoryCache(
+            { sweepDelay: 30 * 1000 * 60 },
+          );
+    this.locationNameByCoordinates =
+      caches && (caches.locationNameByCoordinates === null)
+        ? undefined
+        : caches?.locationNameByCoordinates ||
+          new MemoryCache(
+            { sweepDelay: 30 * 1000 * 60 },
+          );
+    this.currentWeather = caches && (caches.currentWeather === null)
+      ? undefined
+      : caches?.currentWeather ||
+        new MemoryCache(
+          { sweepDelay: 10 * 1000 * 60 },
+        );
+    this.forecast5days3hours = caches && (caches.forecast5days3hours === null)
+      ? undefined
+      : caches?.forecast5days3hours ||
+        new MemoryCache(
+          { sweepDelay: 10 * 1000 * 60 },
+        );
   }
 }
